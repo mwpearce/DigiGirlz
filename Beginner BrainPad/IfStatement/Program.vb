4ae03ee2e2c144ad50bba1fb65eeda073deb8cc6
mwpearce <mwpearce@eastman.com>
2017-04-04 12:43:08 -0400

    Fixes

--- Beginner BrainPad/IfStatement/Program.vb
+++ Beginner BrainPad/IfStatement/Program.vb
@@ -3,13 +3,13 @@ Class Program
         'Put your setup code here. It runs once when the BrainPad starts up.
 
         BrainPad.TrafficLight.TurnGreenLightOff()
-
+        If BrainPad.Button.IsDownPressed() Then
+            BrainPad.TrafficLight.TurnGreenLightOn()
+        End If
     End Sub
 
     Public Sub BrainPadLoop()
         'Put your program code here. It runs repeatedly after the BrainPad starts up.
-        If BrainPad.Button.IsDownPressed() Then
-            BrainPad.TrafficLight.TurnGreenLightOn()
-        End If
+
     End Sub
 End Class
